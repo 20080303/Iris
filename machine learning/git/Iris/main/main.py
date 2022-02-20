c92cf3c89155d11fe44c6676889712855b44720e
CatCat <20080303.o@163.com>
2022-02-20 16:21:14 +0800

    更改了一点点

--- machine learning/git/Iris/main/main.py
+++ machine learning/git/Iris/main/main.py
@@ -2,15 +2,19 @@ from datetime import date
 from telnetlib import X3PAD
 from tkinter import Y
 from matplotlib.cbook import print_cycles
+import numpy as np
 from sklearn import datasets
 from sklearn.datasets import load_iris
 from sklearn.cluster import KMeans
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier 
+import pandas as pd
 iris_dataset = load_iris()
-x_train, x_test, y_train, y_test = train_test_split(iris_dataset['data'], iris_dataset['target'], test_size=0.2,random_state=0)
+x_train, x_test, y_train, y_test = train_test_split(iris_dataset['data'], iris_dataset['target'])
 knn = KNeighborsClassifier(n_neighbors=3)
 knn.fit(x_train,y_train)
-x_pre = knn.predict(x_test)
-print(knn.score(x_test,y_test))
+y_pre = knn.predict(x_test)
+print("expectation:\n {}".format(y_test))
+print("predictions:\n {}".format(y_pre))
+print("score: {:.2f}".format(np.mean(y_pre == y_test)))
